8c45feef3d4230ad65b4edd03045fd722abf8a63
kimberly owens <kimberlyowens@lawn-128-61-33-29.lawn.gatech.edu>
2017-09-28 18:06:23 -0400

    added comments

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ var config = {
 };
 firebase.initializeApp(config);
 
-
+//set variables
 var trainName = "";
 var destination = "";
 var firstTrainTime = 0;
@@ -17,30 +17,32 @@ var frequency = 0;
 var minutesAway = 0;
 var arrivalHumanize = 0;
 var trainArrival = 0;
-
+//capture value from form
 $('#newTrain').on('click',function(eventObject){
+    //prevent pag from refreshing
     eventObject.preventDefault();
     trainName = $('#trainName').val().trim();
     destination = $('#destination').val().trim();
     frequency = $('#frequency').val().trim();
-
+//push data to firebase
     firebase.database().ref().push({
         trainName:trainName,
         destination:destination,
         firstTrainTime: firstTrainTime,
         frequency: frequency,
-
+//add timestamp to acurately track last added child
         dateAdded:firebase.database.ServerValue.TIMESTAMP
     });
-
+//clear form
     $('#form').trigger('reset');
 });
-
+//set variables to equal value captured from firebase
 firebase.database().ref().on('child_added', function(snapshot){
     var trainName = snapshot.val().trainName;
     var destination = snapshot.val().destination;
     var firstTrainTime = snapshot.val().firstTrainTime;
     var frequency = snapshot.val().frequency;
+    //moment calculation
     var firstTrainTimeHumanize = moment(firstTrainTime, "hh:mm").subtract( 1, "years");
     var firstTrainTimeMinutes = moment().diff(moment(firstTrainTimeHumanize),'minutes');
     var firstTrainTimeLeft = firstTrainTimeMinutes % frequency;
@@ -49,7 +51,7 @@ firebase.database().ref().on('child_added', function(snapshot){
     var arrivalHumanize = moment(trainArrival).format('hh:mm');
     console.log(arrivalHumanize);
     console.log(minutesAway);
-
+//append data to HTML
     $('#added-train').append('<tr><td>' + trainName + '</td><td>' + destination + '</td><td>' + frequency + '</td><td>' + arrivalHumanize + '</td><td>' + minutesAway +'</td></tr>');
 
 
